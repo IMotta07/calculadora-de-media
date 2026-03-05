368da8b8b8f2716e00d69721bec28ee92d83ed9e
Thur444 <arthurghp20@gmail.com>
2026-03-05 09:22:12 -0300

    Projeto finalizado

---
+++ app.js
@@ -0,0 +1,24 @@
+const numero1 = document.getElementById("numero1")
+const numero2 = document.getElementById("numero2")
+const button = document.getElementById("button")
+const resultado = document.getElementById("resultado")
+
+
+function calculaMedia(){
+    const nota1 = Number (numero1.value)
+    const nota2 = Number (numero2.value)
+const media = (nota1 + nota2)/2  
+
+
+    console.log (nota1 + nota2)
+
+
+    if(media < 5){
+        resultado.innerHTML = `Sua média foi ${media.toFixed(2)}. Voce esta reprovado`
+    }else{
+        resultado.innerHTML = `Sua media foi ${media.toFixed(2)}.Parabens,voce foi aprovado`
+    }
+}
+
+
+button.addEventListener("click", calculaMedia)
